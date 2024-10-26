cd9bc750a424b44e4094adc19e1967c0a1a8822d
gunashekar22 <146054022+gunashekar22@users.noreply.github.com>
2024-10-26 22:57:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,45 +7,45 @@ import Col from 'react-bootstrap/Col';
 import Profile from './profile';
 import './App.css';
 import Toggle from './toggle';
-
 import Education from './education';
- import Home from "./home.js";
+import Home from './home.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Certification from './certifications.js';
-
 import Skills from './skills.js';
 import Contact from './contact.js';
 import Projects from './projects.js';
 
 function App() {
   return (
-    <Router>
-    <Container>
-      {/* row 1 - toggle */}
-      <Row className="mb-4">
-        <Col xs={12} lg={12} className="text-center">
-          <Toggle />
-        </Col>
-      </Row>
-      {/* row 2 - globe */}
-      <Row className="mb-4">
-        <Col xs={12} lg={12}>
-   
-  
-        </Col>
-      </Row>
-      <Routes>
-      <Route  exact path="/resume" element={<Home/>}/>
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/education" element={<Education />} />
-        {/* <Route path="/certifications" element={<Certification/>}/> */}
-        <Route path="/skills" element={<Skills/>}/>
-        <Route path="/contact" element={<Contact/>}/>
-        <Route path="/projects" element={<Projects/>}/>
-        {/* Add other routes here */}
-      </Routes>
-    </Container>
-  </Router>
+    <Router basename="/resume"> {/* Added basename here */}
+      <Container>
+        {/* Row 1 - Toggle */}
+        <Row className="mb-4">
+          <Col xs={12} lg={12} className="text-center">
+            <Toggle />
+          </Col>
+        </Row>
+        
+        {/* Row 2 - Globe */}
+        <Row className="mb-4">
+          <Col xs={12} lg={12}>
+            {/* Your globe component or content */}
+          </Col>
+        </Row>
+        
+        <Routes>
+          <Route exact path="/resume" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/education" element={<Education />} />
+          {/* Uncomment if you want to use certifications route */}
+          {/* <Route path="/certifications" element={<Certification />} /> */}
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/projects" element={<Projects />} />
+          {/* Add other routes here */}
+        </Routes>
+      </Container>
+    </Router>
   );
 }
 
